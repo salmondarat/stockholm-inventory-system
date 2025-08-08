45800b2d2b28f8661a4dce7d981b927448bd617b
salmondarat <salmonddarat@gmail.com>
2025-08-08 22:09:31 +0700

    Fix: Update/remove default react test for custom content

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Stockholm IMS heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Stockholm IMS/i);
+  expect(headingElement).toBeInTheDocument();
 });
